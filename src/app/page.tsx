a45d9726952fecc0ba3292024141aa1ea0165421
xdtr27 <alexandreconta617@gmail.com>
2024-03-04 18:54:48 -0300

    Listagem de produtos - Parte 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Next E-commerce 14</h1>
+    <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-10 gap-10 xl:gap-6">
+        <div className="bg-white rounded-lg shadow-lg overflow-hidden">prod 1</div>
+        <div className="bg-white rounded-lg shadow-lg overflow-hidden">prod 1</div>
+        <div className="bg-white rounded-lg shadow-lg overflow-hidden">prod 1</div>
+        <div className="bg-white rounded-lg shadow-lg overflow-hidden">prod 1</div>
+      </div>
     </div>
   );
 }
