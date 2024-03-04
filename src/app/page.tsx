96ac40b71cea81b7c7aacf89c69bb28863c56c76
xdtr27 <alexandreconta617@gmail.com>
2024-03-04 19:27:51 -0300

    Definindo tipagem para Produto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,28 @@
-import Image from "next/image";
 
-export default function Home() {
+// https://fakestoreapi.com/products
+
+import { ProductType } from "@/types/ProductType";
+
+
+
+async function getProducts() {
+  const response = await fetch('https://fakestoreapi.com/products');
+  if (!response.ok) throw new Error('failed to get products');
+
+  return response.json();
+}
+export default async function Home() {
+  const products = await getProducts();
+  console.log(products);
+
   return (
     <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-10 gap-10 xl:gap-6">
-        <div className="bg-white rounded-lg shadow-lg overflow-hidden">prod 1</div>
-        <div className="bg-white rounded-lg shadow-lg overflow-hidden">prod 1</div>
-        <div className="bg-white rounded-lg shadow-lg overflow-hidden">prod 1</div>
-        <div className="bg-white rounded-lg shadow-lg overflow-hidden">prod 1</div>
+       {products.map((product: ProductType) => {
+        <div key={product.id}>
+          {product.name}
+        </div>
+       })}
       </div>
     </div>
   );
---
+++ src/types/ProductType.ts
@@ -0,0 +1,8 @@
+export type ProductType = {
+  id: number,
+  name: string,
+  price: number | null,
+  image: string,
+  description: string | null,
+  category: string,
+}
