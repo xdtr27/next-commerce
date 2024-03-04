52206ba9a1c89fd420bb41eca357ca36a2bfe656
xdtr27 <alexandreconta617@gmail.com>
2024-03-04 18:42:05 -0300

    Adicionando Navbar

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav className="fixed top-0 w-full flex items-center py-2 px-8 justify-between z-50 bg-slate-800 text-gray-300">
+      <Link
+        href="#"
+        className="uppercase font-bold text-md h-12 flex items-center"
+      >
+        Next Store
+      </Link>
+    </nav>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
+    <div>
       <h1>Next E-commerce 14</h1>
-    </main>
+    </div>
   );
 }
